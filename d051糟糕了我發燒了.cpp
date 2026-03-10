9b682387fef5cb10d3474c667ed0f6958cec4afa
ibringbst <s1130179@dmhs.kh.edu.tw>
2026-03-10 11:23:43 +0800

    糟糕我發燒程式
    Fix typo in output stream and add temperature conversion.

---
+++ d051糟糕了我發燒了.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : apcs.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// ===========================================================================
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int  f;
+	cin >> f;
+	cont << fixed;
+	cont << setprecision(3) << (f-32)*5/9 <<"\n";
+	return 0;
+}
